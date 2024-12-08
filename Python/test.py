36a9f45ea968b7f477580a6f06d46ca0f3c1bb22
Antony Huang <antony@ms56.url.com.tw>
2024-12-08 18:59:03 +0800

    Add PySide6 function test

--- Python/test.py
+++ Python/test.py
@@ -1,4 +1,20 @@
+import sys
+import PySide6.QtCore
+from PySide6.QtWidgets import QApplication, QLabel
+
+# Prints PySide6 version
+print(PySide6.__version__)
+
+# Prints the Qt version used to compile PySide6
+print(PySide6.QtCore.__version__)
+
 """Module providingFunction print test."""
 print("Test Python Program ~")
 print("What happen ?")
-input()
+# input()
+
+app = QApplication(sys.argv)
+label = QLabel("Hello World!")
+label.resize(800, 600)
+label.show()
+app.exec()
